7f657765fb445312ea96ddabc9dd62d20bdc390a
pgerasimov <gerasimov86@gmail.com>
2020-05-16 01:32:13 +0300

    scatter to figure

--- webapp/grapf.py
+++ webapp/grapf.py
@@ -6,14 +6,13 @@ import pandas as pd
 def get_grapf(all_data):
     rub_data = {}
     kB_data = {}
-    
 
     for item in all_data:
         rub_data[item.date] = item.summ
         kB_data[item.date] = item.t1 + item.t2
 
-    fig = plt.scatter(x=kB_data, y = rub_data, figsize=(14, 10), trendline="ols")
-
+    fig = plt.figure(figsize=(14, 10))
+    
     x1 = plt.subplot(321)
     x1.title.set_text('Расход в кВ')
     kB_plot = pd.Series(kB_data)
@@ -22,7 +21,6 @@ def get_grapf(all_data):
     x2 = plt.subplot(322)
     x2.title.set_text('Расход в рублях')
     rub_plot = pd.Series(rub_data)
-
     rub_plot.plot()
 
     # fig.savefig('webapp/static/img/stat.png')
