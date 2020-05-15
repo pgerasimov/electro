59d1f91b3003d3b9122c433a25d2a867fd0daf54
pgerasimov <gerasimov86@gmail.com>
2020-05-16 01:18:27 +0300

    up dv2

--- webapp/add_item.py
+++ webapp/add_item.py
@@ -4,7 +4,7 @@ from webapp.model import db, electro
 app = create_app()
 
 with app.app_context():
-    new_record = electro(t1=1000, t2=1100, tarif1=6.65, tarif2=2.51, date='07/20', summ=40000)
+    new_record = electro(t1=2000, t2=1000, tarif1=6.65, tarif2=2.51, date='09/20', summ=4000)
 
     db.session.add(new_record)
     db.session.commit()
--- webapp/grapf.py
+++ webapp/grapf.py
@@ -13,10 +13,9 @@ def get_grapf(all_data):
         rub_data[item.date] = item.summ
         kB_data[item.date] = item.t1 + item.t2
 
-    print(rub_data)
-    print(kB_data)
 
     fig = plt.figure(figsize=(14, 10))
+
     x1 = plt.subplot(321)
     x1.title.set_text('Расход в кВ')
     kB_plot = pd.Series(kB_data)
@@ -25,13 +24,10 @@ def get_grapf(all_data):
 
     x2 = plt.subplot(322)
     x2.title.set_text('Расход в рублях')
-
     rub_plot = pd.Series(rub_data)
     rub_plot.plot()
 
-
-
-    fig.savefig('webapp/static/img/stat.png')
+    # fig.savefig('webapp/static/img/stat.png')
 
     fig_html = mpld3.fig_to_html(fig)
 
