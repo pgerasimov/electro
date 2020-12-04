cf419d2ca0595d2636cbb1f3f2fd330a2a2442fe
pgerasimov <gerasimov86@gmail.com>
2020-12-04 09:28:50 +0300

    new data

--- webapp/add_item.py
+++ webapp/add_item.py
@@ -10,6 +10,10 @@ with app.app_context():
     new_record3 = electro(t1=2833, t2=1332, tarif1=6.65, tarif2=2.51, date='07/20', summ=4491)
     new_record4 = electro(t1=3339, t2=1613, tarif1=6.8, tarif2=2.61, date='08/20', summ=4174)
     new_record5 = electro(t1=3635, t2=1826, tarif1=6.8, tarif2=2.61, date='09/20', summ=2568)
+    new_record6 = electro(t1=4337, t2=2215, tarif1=6.8, tarif2=2.61, date='10/20', summ=5689)
+
+
+
 
     db.session.add(new_record)
     db.session.add(new_record1)
@@ -21,5 +25,7 @@ with app.app_context():
     db.session.add(new_record4)
 
     db.session.add(new_record5)
+    db.session.add(new_record6)
+
 
     db.session.commit()
